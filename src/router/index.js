6857453f289278bc38c756bbd1ccb639fb1b83e8
fahimnuman <fahiomnuman87@gmail.com>
2023-02-15 19:13:36 +0600

    added sotre,router index ,home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import HomePage from '../components/pages/HomePage';
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
-    {
-        name: 'Home',
-        path: '/',
-        component: HomePage
-    }
-];
+  {
+    name: 'Home',
+    path: '/',
+    component: HomePage
+  }
+  
+
+]
 
 const router = new VueRouter({
-    mode: 'history',
-    base: 'localhost:8080',
-    routes
-});
+  mode: "history", //html file history mode
+  base: process.env.BASE_URL,
+  routes
+})
 
 export default router;
